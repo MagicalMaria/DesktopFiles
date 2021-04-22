0741aef550d895e1c0d8dd04db39b6932a053c55
MagicalMaria <mariaantonydgl@gmail.com>
2021-04-22 17:47:48 +0530

    Final New Part 1 Commit

--- New/Downloads/NCRemediationLaptopInstallAllSCCMCN1_ScriptExecute/NCRemediationLaptopInstallAllSCCMCN1_ScriptExecute/TestAzure.ps1
+++
@@ -1,9 +0,0 @@
-$verify = Test-Path $('filesystem::\\cognizantonline.sharepoint.com@SSL\DavWWWRoot\teams\ITSoftRepo\Shared Documents\EPTAgents')
-if($verify)
-{
-write-host "1"
-}
-Else
-{
-write-host "0"
-}
